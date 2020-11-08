3e55584b97e32b34c450b8bd81cdf4ef97103ce3
Juan Diego Ocampo <jd.ocampom0117@icloud.com>
2020-11-07 19:32:08 -0500

    Initial Commit

---
+++ ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Juan Diego Ocampo on 07/11/2020.
+//  Copyright © 2020 Juan Diego Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+// MARK: Methods
+
+    /// Tag: viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Juan Diego Ocampo on 07/11/2020.
+//  Copyright © 2020 Juan Diego Ocampo. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+// MARK: Variables
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+}
